18943ec39193dc77fda4e9fd1d62c3885b36a7a8
shovalsolo <shovalso@gmail.com>
2017-07-17 11:30:45 -0700

    Mobile ready 17-7-17
    Mobile ready 17-7-17

---
+++ addingToCart.js
@@ -0,0 +1,24 @@
+//
+
+// var p = 0;
+
+// function addCart() {
+	// if(p==1){
+		// p=0;
+		// document.getElementById('cart').innerHTML="0";
+		
+	// }else{
+		// p=1;
+		// document.getElementById('cart').innerHTML="1";
+		
+	// }
+
+// }
+
+var p1=0;
+
+function addCart() {
+p1++;
+document.getElementById("cart").innerHTML=p1;
+
+}
