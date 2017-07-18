b4d401352eda1915f3764ceeb863f063750f34de
shovalsolo <shovalso@gmail.com>
2017-07-18 11:35:40 -0700

    11:30 update

--- bx-script.js
+++ bx-script.js
@@ -1,8 +1,22 @@
-$('.bxslider').bxSlider({
-  mode: 'fade',
-  captions: true,
-  slideMargin: 10,
-  minSlides: 1,
-  maxSlides: 4
-    
-});
+if ($(window).width() < 600) {
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      //slideWidth: 400,
+      minSlides: 1,
+      maxSlides: 1
+    });
+}else if ($(window).width() < 1000){
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      //slideWidth: 400,
+      minSlides: 2,
+      maxSlides: 2
+    });
+}else{
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      //slideWidth: 400,
+      minSlides: 4,
+      maxSlides: 4
+    });
+}
